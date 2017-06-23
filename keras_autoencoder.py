2767175a1f1f295f3e0b0931140c5865bd331078
bl-2633 <bl2633@columbia.edu>
2017-06-23 18:12:59 -0400

    added models and test

---
+++ keras_autoencoder.py
@@ -0,0 +1,107 @@
+from keras.layers import Dense
+from keras.models import Model, Sequential
+import data_import
+import pickle as pkl
+import json
+data = data_import.import_data("~/Dropbox/missense_pred/data/john/HIS_input_data.csv",test = 1)
+
+
+X_train = data[0]['X_train']
+X_test = data[0]['X_test']
+y_train = data[1]['y_train']
+y_test = data[1]['y_test']
+
+Model1 = Sequential()
+Model1.add(Dense(30,input_shape = (40,),activation = 'relu',name = 'inter1'))
+Model1.add(Dense(40,activation = 'relu'))
+
+Model1.compile(optimizer = 'adam',loss = 'mean_squared_error')
+Model1.fit(X_train,X_train,epochs = 100,batch_size = 1000,validation_data = (X_test,X_test))
+
+weight_1 = Model1.get_weights()[0]
+with open("weight_1",'wb') as f:
+    pkl.dump(weight_1,f)
+
+middle1 = Model(inputs = Model1.input,outputs = Model1.get_layer('inter1').output)
+middle1_out = middle1.predict(X_train)
+middle1_out_test = middle1.predict(X_test)
+
+
+
+Model2 = Sequential()
+Model2.add(Dense(25,input_shape = (30,),activation = 'relu',name = 'inter2'))
+Model2.add(Dense(30,activation = 'relu'))
+Model2.compile(optimizer = 'adam',loss = 'mean_squared_error')
+Model2.fit(middle1_out,middle1_out,epochs = 100,batch_size = 1000, validation_data = (middle1_out_test,middle1_out_test))
+
+weight_2 = Model2.get_weights()[0]
+with open("weight_2",'wb') as f:
+    pkl.dump(weight_2,f)
+
+
+middle2 = Model(inputs = Model2.input, outputs = Model2.get_layer('inter2').output)
+middle2_out = middle2.predict(middle1_out)
+middle2_out_test = middle2.predict(middle1_out_test)
+
+
+Model3 = Sequential()
+Model3.add(Dense(20,input_shape = (25,),activation = 'relu',name = 'inter3'))
+Model3.add(Dense(25,activation = 'relu'))
+Model3.compile(optimizer = 'adam',loss = 'mean_squared_error')
+Model3.fit(middle2_out,middle2_out,epochs = 100,batch_size = 1000, validation_data = (middle2_out_test,middle2_out_test))
+
+weight_3 = Model3.get_weights()[0]
+with open("weight_3",'wb') as f:
+    pkl.dump(weight_3,f)
+
+# initializer for pre-trained weights
+def my_init1(shape, dtype = None):
+    return weight_1
+
+def my_init2(shape,dtype = None):
+    return weight_2
+
+def my_init3(shape,dtype = None):
+    return weight_3
+
+
+mlp = Sequential()
+mlp.add(Dense(30,input_shape = (40,),activation = 'relu',kernel_initializer = my_init1))
+mlp.add(Dense(25,activation ='relu',kernel_initializer = my_init2))
+mlp.add(Dense(20,activation = 'relu',kernel_initializer = my_init3))
+mlp.add(Dense(1,activation = 'sigmoid'))
+mlp.compile(loss = 'binary_crossentropy',
+		optimizer = 'adam',
+		metrics = ['accuracy'])
+
+mlp.fit(X_train,y_train,epochs = 100,batch_size = 200)
+SAE_struct = mlp.to_json()
+with open("ASE_arc.txt",'w') as f:
+    json.dump(SAE_struct,f)
+mlp.save_weights('ASE_weights.h5')
+
+#building the autoencoder
+'''
+encoding_dim = 30
+input_data = Input(shape = (40,))
+encoded = Dense(encoding_dim,activation = 'relu')(input_data)
+decoded = Dense(40,activation = 'relu')(encoded)
+
+autoencoder = Model(input_data,decoded)
+
+encoder = Model(input_data,encoded)
+
+encoded_input = Input(shape=(encoding_dim,))
+# retrieve the last layer of the autoencoder model
+decoder_layer = autoencoder.layers[-1]
+# create the decoder model
+decoder = Model(encoded_input, decoder_layer(encoded_input))
+
+autoencoder.compile(optimizer = 'adam',loss = 'mean_squared_error')
+
+autoencoder.fit(X_train,X_train,epochs = 100,batch_size = 1000,validation_data = (X_test,X_test))
+'''
+
+
+
+
---
+++ predict.py
@@ -0,0 +1,66 @@
+from keras.models import Sequential
+import data_import
+from keras.layers import Dense
+import matplotlib.pyplot as plt
+from sklearn import metrics
+import pandas as pd
+model = Sequential()
+model.add(Dense(30,input_shape = (40,),activation = 'relu'))
+model.add(Dense(25,activation ='relu'))
+model.add(Dense(20,activation = 'relu'))
+model.add(Dense(1,activation = 'sigmoid'))
+
+model.load_weights("ASE_weights.h5")
+
+def plot_roc(df, y_true, label):
+
+    fpr = dict()
+    tpr = dict()
+    roc_auc = dict()
+    algos = ["SAE_prob",'MetaSVM_rankscore', 'MetaLR_rankscore', 'M-CAP_rankscore',
+             'DANN_rankscore','CADD_phred', 'Eigen-phred','Polyphen2_HVAR_rankscore',
+             'MutationTaster_converted_rankscore', 'FATHMM_converted_rankscore',
+             'fathmm-MKL_coding_rankscore']
+    for algo in algos:
+        index = df[algo]!= -1
+        y_score = df.ix[index][algo].values
+        y_true_nomissing = y_true[index]
+
+        fpr[algo], tpr[algo], _ = metrics.roc_curve(y_true_nomissing, y_score)
+        roc_auc[algo] = metrics.auc(fpr[algo], tpr[algo])
+    # jump comes from missing value
+
+    plt.figure(figsize = (10,10))
+    lw = 2
+    for algo in algos:
+        plt.plot(fpr[algo], tpr[algo], lw=lw,
+                 label='{} ROC curve (area = {:.2f})'.format(algo, roc_auc[algo]))
+
+        plt.plot([0, 1], [0, 1], color='navy', lw=lw, linestyle='--')
+        plt.xlim([0.0, 1.0])
+        plt.ylim([0.0, 1.05])
+        plt.xlabel('False Positive Rate')
+        plt.ylabel('True Positive Rate')
+
+    total = len(y_true)
+    pos = sum(y_true)
+    neg = total - pos
+
+    plt.title('Receiver operating characteristic of {}: {} positive, {} negative'.format(label, pos, neg))
+    plt.legend(loc="lower right", fontsize = 'medium')
+    plt.show()
+
+fname = "/Users/bindy/Dropbox/missense_pred/data/john/HIS_metaSVM_addtest2.anno.rare.reformat.csv"
+fname2 = "/Users/bindy/Dropbox/missense_pred/data/cancer_hotspots/hotspot.anno.rare.reformat.csv"
+data = data_import.import_data(fname)
+data2 = data_import.import_data(fname2)
+X_train = data[0]['X_train']
+X_test = data[0]['X_test']
+y_train = data[1]['y_train']
+y_test = data[1]['y_test']
+df = pd.read_csv(fname)
+y_true = df.pop('target')
+y_score = model.predict_proba(X_test,batch_size = 20, verbose = 1)
+df = df.assign(SAE_prob = y_score)
+plot_roc(df,y_true,label = "HS_metaSVM_addtest2")
+
